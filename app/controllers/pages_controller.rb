6c6be7edd4086aa0a53d6952af93778061230811
Christian Morris <cjmorris96@hotmail.com>
2023-08-22 18:09:34 +0800

    strong params added for devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: :profile
+
   def home
+  end
+
+  def profile
+    @user = User.find(current_user.id)
   end
 end
